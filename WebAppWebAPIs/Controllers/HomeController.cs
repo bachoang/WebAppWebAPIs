21d17cbddddeb1ccd17d2caf9a393e35257efe8c
Bac Hoang <bachoang@microsoft.com>
2022-04-05 17:05:52 -0500

    Add project files.

---
+++ WebAppWebAPIs/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+using System.Diagnostics;
+using WebAppWebAPIs.Models;
+
+namespace WebAppWebAPIs.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        readonly ITokenAcquisition tokenAcquisition;
+        private IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, ITokenAcquisition tokenAcquisition, IConfiguration configuration)
+        {
+            _logger = logger;
+            this.tokenAcquisition = tokenAcquisition;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AuthorizeForScopes(ScopeKeySection = "GraphScope:CalledApiScopes")]
+        public async Task<IActionResult> Graph()
+        {
+
+            string[] scopes = _configuration.GetValue<string>("GraphScope:CalledApiScopes").Split(" ");
+
+            string token = await tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+
+            ViewBag.GraphToken = token;
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebAppWebAPIs/Controllers/WebAPIController1.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web;
+
+namespace WebAppWebAPIs.Controllers
+{
+    // https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/adding-controller?view=aspnetcore-6.0&tabs=visual-studio
+    [Authorize]
+    public class WebAPIController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        readonly ITokenAcquisition tokenAcquisition;
+        private IConfiguration _configuration;
+        public WebAPIController(ILogger<HomeController> logger, ITokenAcquisition tokenAcquisition, IConfiguration configuration)
+        {
+            _logger = logger;
+            this.tokenAcquisition = tokenAcquisition;
+            _configuration = configuration;
+        }
+
+        [AuthorizeForScopes(ScopeKeySection = "ApiScope:CalledApiScopes")]
+        public async Task<IActionResult> Api()
+        {
+            string[] scopes = _configuration.GetValue<string>("ApiScope:CalledApiScopes").Split(" "); ;
+            string token = await tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+            ViewBag.ApiToken = token;
+            return View();
+        }
+        public IActionResult Index()
+        // public string Index()
+        {
+            return View();
+            // return "This is my default action...";
+        }
+    }
+}
